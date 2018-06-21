2b1d8a110bc8f870b67f25161207361361143ad7
ParkJinsu(school) <pwlstn@naver.com>
2018-06-21 15:21:51 +0900

    실습2

---
+++ Java/CH04/src/PrefixOp.java
@@ -0,0 +1,18 @@
+
+public class PrefixOp {
+
+	public static void main(String[] args) {
+		int num = 7;
+		System.out.println(num++);
+		System.out.println(num++);
+		System.out.println(num);
+		
+		System.out.println("--------------------");
+		
+		int num2 = 5;
+		System.out.println(++num);
+		System.out.println(++num);
+		System.out.println(num);
+	}
+
+}
